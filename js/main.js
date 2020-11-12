63050f08573ef63fc7b23989163121a41b835505
Tatyana Shurinova <61487027+tanne-sh@users.noreply.github.com>
2020-11-12 10:02:21 +0200

    add EventListener

--- js/main.js
+++ js/main.js
@@ -9,3 +9,46 @@ menuToggle.addEventListener('click', function (event) {
   // вешаем класс на меню, когда кликнули по кнопке меню 
   menu.classList.toggle('visible');
 })
+
+
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const emailInput = document.querySelector('.login-email');
+const passwordInput = document.querySelector('.login-pasword');
+const loginSignup = document.querySelector('.login-signup');
+
+const listUsers = [
+  {
+    email: 'PiedPiper@gmail.comPiedPiper@gmail.com',
+    password:'zxc',
+    displayName: 'Pied Piper'
+  },
+  {
+    email: 'BertramGilfoyle@gmail.com',
+    password:'qwery',
+    displayName: 'Bertram Gilfoyle'
+  }
+];
+
+const setUsers = {
+  user: null,
+  logIn() {
+    console.log('вход')    
+  },
+  logOut() {
+    console.log('выход')
+  },
+  signUp() {
+    console.log('регистрация')    
+  }
+};
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  setUsers.logIn(emailInput.value, passwordInput);
+});
+
+loginSignup.addEventListener('click', event => {
+  event.preventDefault();
+  setUsers.signUp();
+});
